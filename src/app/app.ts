e12c39e2244b4ef8e2d67fee15a2f9a57d6b6925
odgm <ordyelizkh@gmail.com>
2026-06-25 01:22:55 -0300

    boton sumar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Operador } from "./operador/operador";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Operador],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/operador/operador.ts
+++ src/app/operador/operador.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-operador',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './operador.html',
   styleUrl: './operador.css',
 })
-export class Operador {}
+export class Operador {
+  operador1 = 0;
+  operador2 = 0;
+  resul = 0;
+
+  resultado() {
+    this.resul = (this.operador1 + this.operador2);
+  }
+}

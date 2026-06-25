b27746b165be10d4c0d98c669c0119fe80d12445
odgm <ordyelizkh@gmail.com>
2026-06-25 00:35:00 -0300

    preparando los archivos

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('calculadora');
+  protected readonly title = 'Aplicación Calculadora';
 }
---
+++ src/app/operador/operador.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operador',
+  imports: [],
+  templateUrl: './operador.html',
+  styleUrl: './operador.css',
+})
+export class Operador {}
